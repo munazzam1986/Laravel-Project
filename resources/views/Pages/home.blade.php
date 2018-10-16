58694a8d0c77a3580c89c3fb99b1442f89fa7766
munazzam <munazzamm@gmail.com>
2018-10-16 16:34:38 -0400

    Adding folders Layouts, Pages, Includes

---
+++ resources/views/Pages/home.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts.home')
+@section('content')
+
+        <h1>Hello Welcome to my new Laravel Homepage</h1>
+@stop
---
+++ resources/views/layouts/default.blade.php
@@ -0,0 +1,29 @@
+<!doctype html>
+<html lang="en">
+<head>
+    @include('includes.head')
+</head>
+<body>
+
+<header>
+@include('includes.head')
+</header>header>
+
+<main role=""main">
+@yield('content')
+        <footer class="row">
+
+        </footer>
+</main>
+
+<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"
+        integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN"
+        crossorigin="ananymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.minjs"
+        integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q"
+        crossorigin="ananymous"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"
+        integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUae5+76PVCmYl"
+        crossorigin="ananymous"></script>
+</body>
+</html>

2b46dc994bf03f2f54a1af19e82dc4bfc8b36314
munazzam <munazzamm@gmail.com>
2018-10-16 23:02:55 -0400

    Final version. Includes head error stays

--- resources/views/Includes/footer.blade.php
+++ resources/views/Includes/footer.blade.php
@@ -1,5 +1,5 @@
 <footer class="container">
     <p class="float-right"><a href="#">Back to top</a>/p>
-    <p>&copy; 2017-2018 Learana, Inc. &middot;<a href="#">Privacy</a> &middot;<a herf="#">Terms</a> </p>
+    <p>&copy; Munazzam Mirza &middot;<a href="#">Privacy</a> &middot;<a herf="#">Terms</a> </p>
 </footer>
 

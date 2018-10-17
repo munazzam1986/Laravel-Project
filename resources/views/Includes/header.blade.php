630fda0be1172350c6d30ce4802ab6046d5ebfbe
munazzam <munazzamm@gmail.com>
2018-10-16 20:38:25 -0400

    Updated code 2

--- resources/views/Includes/header.blade.php
+++ resources/views/Includes/header.blade.php
@@ -13,29 +13,9 @@
                         <a class="nav-link" href="/">Home <span class="sr-only">(current)</span></a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="/discover-online-courses">Discover</a>
-                    </li>
-                    <li class="nav-item">
-
-                        <a class="nav-link" href="/why-learana">Why Learana?</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="/meet-learana">Meet Learana</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="/connect">Connect</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="/login">Login</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="/get-started">Get Started</a>
+                        <a class="nav-link" href="/about">About</a>
                     </li>
                 </ul>
-                <form class="form-inline my-2 my-lg-0">
-                    <input class="form-control mr-sm-2" type="text" placeholder="Search">
-                    <button class="btn btn btn-success my-2 my-sm-0" type="submit">Find Courses</button>
-                </form>
             </div>
         </div>
     </nav>

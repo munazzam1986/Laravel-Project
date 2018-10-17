71655bbb7161cf80143bc9cbb027052beecc68d5
munazzam <munazzamm@gmail.com>
2018-10-16 20:54:00 -0400

    Updated code 2

--- resources/views/Includes/header.blade.php
+++ resources/views/Includes/header.blade.php
@@ -1,7 +1,7 @@
 <header>
     <nav class="navbar navbar-expand-sm navbar-dark bg-primary fixed-top">
         <div class="container">
-            <a class="navbar-brand" href="/">Learana</a>
+            <a class="navbar-brand" href=" /">Laravel Assignment</a>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse"
                     aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
